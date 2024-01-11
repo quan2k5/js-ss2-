304b1ba35adfd22863bef084ed0bbf615f67d2b1
quan2k5 <148734034+quan2k5@users.noreply.github.com>
2024-01-12 06:25:11 +0700

    Add files via upload

---
+++ js-câu-điều-kiện/demo.js
@@ -0,0 +1,23 @@
+//var demoHoisting:
+//1-cơ chế hoạt động của javascript trên trình duyệt ?
+//2-thế nào là javascript runtime ?-kể tên
+//3-thế nào là javascript engineer ?-kể tên
+//4-thế nào là TDZ(temparel Dend Zone) ?
+console.log(demoHoisting);
+var demoHoisting="đây là biến bị hoisting"
+console.log(demoHoisting);
+//console.log(fname);
+//console.log(age);
+let fname="anh quan";
+//const age=19;
+demoHoisting="biến demoHoisting sau khi đc gán";
+fname="sỹ hiệp";
+let AnhQuan=true;
+const VladimerPutin=false;
+var stauts=true;
+const age=false;
+console.log(1=='1'); //so sánh tương đối
+console.log(1==='1');//so sánh tuyệt đối
+
+
+
